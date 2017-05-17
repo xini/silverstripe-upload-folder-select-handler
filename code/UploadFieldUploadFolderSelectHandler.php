23dd6787f8b77b0f2a41f1814cecb0e7f4a93d50
Florian Thoma <florian.thoma@innoweb.com.au>
2017-05-17 14:58:06 +1000

    fix logic around folder loading

--- code/UploadFieldUploadFolderSelectHandler.php
+++ code/UploadFieldUploadFolderSelectHandler.php
@@ -2,12 +2,9 @@
 class UploadFieldUploadFolderSelectHandler extends UploadField_SelectHandler {
     
     protected function getListField($folderID) {
-		$requestedFolderID = $this->parent->getRequest()->requestVar('ParentID');
-		if (
-		    $requestedFolderID === null 
-		    && !$this->parent->getDisplayFolderName() 
-		    && $this->parent->getFolderName() != Config::inst()->get('Upload', 'uploads_folder')
-		) {
+		// set folder to FolderName if no no DisplayFolder is set explicitly
+        $folderID = $folderID ? $folderID : $this->parent->getRequest()->requestVar('ParentID');
+		if (!$this->parent->getDisplayFolderName() || strlen($this->parent->getDisplayFolderName()) == 0) { 
 			$folder = Folder::find_or_make($this->parent->getFolderName());
 			$folderID = $folder ? $folder->ID : 0;
 		}
