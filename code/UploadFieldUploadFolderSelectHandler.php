dc42fa2bc85c28dde8fe66a93497fc5cde58c8bb
Florian Thoma <florian.thoma@innoweb.com.au>
2017-06-13 14:13:41 +1000

    fix folder selection if folder given

--- code/UploadFieldUploadFolderSelectHandler.php
+++ code/UploadFieldUploadFolderSelectHandler.php
@@ -3,11 +3,11 @@ class UploadFieldUploadFolderSelectHandler extends UploadField_SelectHandler {
     
     protected function getListField($folderID) {
 		// set folder to FolderName if no no DisplayFolder is set explicitly
-        $folderID = $folderID ? $folderID : $this->parent->getRequest()->requestVar('ParentID');
-		if (!$this->parent->getDisplayFolderName() || strlen($this->parent->getDisplayFolderName()) == 0) { 
-			$folder = Folder::find_or_make($this->parent->getFolderName());
-			$folderID = $folder ? $folder->ID : 0;
-		}
+        $folderID = $this->parent->getRequest()->requestVar('ParentID');
+        if ($folderID === null && !$this->parent->getDisplayFolderName() && $this->parent->getFolderName()) {
+            $folder = Folder::find_or_make($this->parent->getFolderName());
+            $folderID = $folder ? $folder->ID : 0;
+        }
         return parent::getListField($folderID);
     }
 }
